e24b7589f9448d52b6b445042783a617de8ae51e
Abraham Herrera <50112365+municipalvice@users.noreply.github.com>
2021-02-16 22:44:59 -0600

    click to toggle mute/unmute

--- public/main.js
+++ public/main.js
@@ -1,25 +1,26 @@
-const tag = document.createElement('script');
 const start_button = document.getElementById('start-button');
+const channel_dial = document.getElementById('channel-dial');
+const volume_dial  = document.getElementById('volume-dial');
+const tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// const doom_videos = [
-//     {
-//         shortCode:"CRHyBwNtkLA",
-//         songName:"Doomsday",
-//     },{
-//         shortCode: "rpaonSDPw7Y",
-//         songName: "Accordion"
-//     }, {
-//         shortCode:"Pb1E5XNheqw",
-//         songName:"Questions"
-//     }
-// ];
-var state;
-var player;
+const doom_videos = [
+    {
+        shortCode:"CRHyBwNtkLA",
+        songName:"Doomsday",
+    },{
+        shortCode: "rpaonSDPw7Y",
+        songName: "Accordion"
+    }, {
+        shortCode:"Pb1E5XNheqw",
+        songName:"Questions"
+    }
+];
 
+var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         videoId: 'CRHyBwNtkLA',
@@ -29,6 +30,7 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
+var state;
 function getState() {
     state = player.getPlayerState();
 }
@@ -44,4 +46,16 @@ start_button.addEventListener('click', _ => {
         player.pauseVideo();
         start_button.innerHTML = 'S T A R T';
     }
+});
+channel_dial.addEventListener('click', _ => {
+    //TODO click to change to random Doom video
+});
+volume_dial.addEventListener('click', _ => {
+   if (!player.isMuted()){
+       player.mute();
+       volume_dial.style.transform = "rotate(-115deg)";
+   } else {
+       player.unMute();
+       volume_dial.style.transform = "rotate(115deg)";
+   }
 });
